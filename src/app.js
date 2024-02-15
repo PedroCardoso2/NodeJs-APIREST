23be1a5d0e14a69d3c955c2d235beb9e048b8b79
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2024-02-15 19:54:22 -0300

    Put

--- src/app.js
+++ src/app.js
@@ -15,6 +15,12 @@ const livros = [
     }
 ]
 
+function buscaLivro(id){
+    return livros.findIndex(livros => {
+        return livros.id === Number(id);
+    })
+}
+
 
 app.get("/", (req,res) => {
     res.status(200).send("Curso Node.js"); 
@@ -24,9 +30,23 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+
+    res.status(200).json(livros[index]);
+})
+
 
 app.post("/livros", (req,res) => {
     livros.push(req.body);
     res.status(201).send("Livro Cadastrado com Sucesso");
 });
+
+app.put("/livros/:id", (req,res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+
+    res.status(200).json(livros[index]);
+})
+
 export default app;
